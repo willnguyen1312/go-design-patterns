0384af6e50c9b4a538599d300e533fa93a04fcaa
Nam Nguyen <nam.nguyen.node@gmail.com>
2019-12-23 11:24:07 +0700

    Add Composite Pattern

---
+++ composite_test.go
@@ -0,0 +1,17 @@
+package patterns
+
+func ExampleBox() {
+	file1 := &File{Name: "file1"}
+	folder1 := &Box{Name: "folder1"}
+	folder2 := &Box{Name: "folder2"}
+
+	folder1.Add(file1)
+	folder2.Add(folder1)
+
+	folder2.Search("Aha")
+
+	// Output:
+	// 	Serching recursively for Aha in box folder2
+	// Serching recursively for Aha in box folder1
+	// Searching for item Aha in file file1
+}
